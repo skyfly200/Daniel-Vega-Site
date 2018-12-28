a781b8e40ec5f8c2e9443ca06a7fa1892c20905f
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-12-28 02:35:35 -0700

    fix other components title

--- .vuepress/theme/EventPage.vue
+++ .vuepress/theme/EventPage.vue
@@ -4,7 +4,7 @@
     <v-content>
       <v-container fluid>
         <v-layout class="pa-4">
-          <Event />
+          <EventItem />
         </v-layout>
       </v-container>
     </v-content>
