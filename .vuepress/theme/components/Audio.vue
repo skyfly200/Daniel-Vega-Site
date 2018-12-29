92fa6c9c165436f833d59688f065180b9d772eaf
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-12-28 23:03:22 -0700

    fix alt link on audio player

--- .vuepress/theme/components/Audio.vue
+++ .vuepress/theme/components/Audio.vue
@@ -1,6 +1,6 @@
 <template>
   <audio :src="frontmatter.file" controls>
-    Your browser does not support the audio element. Here is a <a :href="$page.frontmatter.file">link to the audio</a> instead.
+    Your browser does not support the audio element. Here is a <a :href="frontmatter.file">link to the audio</a> instead.
   </audio>
 </template>
 <script>
